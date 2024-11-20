bb7e8c525a083011515724bf6f4ed7afb451e5cf
Ezequiel Arevalo <eze.m.arevalo@gmail.com>
2024-11-20 20:30:39 +0000

    adding compose

--- routes/streaming.py
+++ routes/streaming.py
@@ -2,19 +2,24 @@ import os
 from pathlib import Path
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse, HTMLResponse
+from urllib.parse import unquote
+from config import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(tags=["Stream output"], prefix="/stream")
 
-@router.get("/prediction")
-async def stream_output():
+@router.get("/prediction/{video_id}")
+async def stream_output(video_id: str):
 
-    paths = sorted(Path("output/").iterdir(), key=os.path.getmtime)
-
+    #paths = sorted(Path(f"output/").iterdir(), key=os.path.getmtime)
+    v_name = f"output/{unquote(video_id)}"
+    logger.debug(f"Streaming {v_name}")
     def iterfile():   
-        with open(paths[0], mode="rb") as file_like:   
+        with open(v_name, mode="rb") as file_like:   
             yield from file_like
 
-    return StreamingResponse(iterfile(), media_type="video/avi")
+    return StreamingResponse(iterfile(), media_type="video/webm")
 
 @router.get("/original")
 async def stream_output():
@@ -24,16 +29,42 @@ async def stream_output():
 
     return StreamingResponse(iterfile(), media_type="image/jpeg")
 
-@router.get("/")
-async def stream_output():
-    html_content = '''
+@router.get("/video/{item_id}")
+async def stream_output(item_id: str):
+    html_content = f'''
     <html>
         <head>
             <title>Some HTML in here</title>
 
         </head>
         <body>
-            <video preload="metadata" id="video" src="http://localhost:8081/stream/prediction" autoplay="autoplay" />
+            <video preload="metadata" id="video" src="http://localhost:8081/stream/prediction/{item_id}" autoplay="autoplay" />
+        </body>
+    </html>
+    '''
+    return HTMLResponse(content=html_content, status_code=200)
+
+@router.get("/")
+async def stream_output():
+    paths = sorted(Path(f"output/").glob("*.webm"), key=os.path.getmtime)    
+
+    html_content = f'''
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+    '''
+
+    for path in paths:
+        video_name=str(path).split("/")[-1]
+        html_content += f'''
+        <p>
+            <a href="/stream/video/{video_name}">{video_name}</>
+        </p>
+        '''
+
+    html_content += '''
         </body>
     </html>
     '''
